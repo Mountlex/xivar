965da36a5dba37b9923275780f42e39984608526
Alexander Lindermayr <alexander.lindermayr97@gmail.com>
2022-03-11 22:49:44 +0100

    wip

--- src/cli/interactive.rs
+++ src/cli/interactive.rs
@@ -1,12 +1,19 @@
 use crate::{
-    remotes::{self, Remote},
+    remotes::{self, PaperHit, Remote},
     Query,
 };
 use anyhow::Result;
-use async_std::task;
+use async_std::{
+    channel::{Receiver, Sender},
+    task,
+};
+use log::warn;
+
+use std::sync::{Arc, Mutex, RwLock};
+
 use clap::Parser;
-use std::io::Write;
-use termion::{clear, cursor, event::Key, input::TermRead, raw::IntoRawMode};
+use std::{fmt::Display, io::Write};
+use termion::{clear, color, cursor, event::Key, input::TermRead, raw::IntoRawMode};
 
 use super::Command;
 
@@ -18,32 +25,40 @@ impl Command for Interactive {
     fn run(&self) -> Result<()> {
         let mut stdin = std::io::stdin().keys();
         let mut stdout = std::io::stdout().into_raw_mode()?;
-        let (sender, receiver) = async_std::channel::unbounded();
+        let (command_sender, command_receiver): (Sender<String>, Receiver<String>) =
+            async_std::channel::unbounded();
 
-        let local_rec = receiver.clone();
+        let (hit_sender, hit_receiver): (Sender<Vec<PaperHit>>, Receiver<Vec<PaperHit>>) =
+            async_std::channel::unbounded();
 
-        let mut state: State = State::Searching;
         let mut data = StateData::new();
 
         write!(stdout, "{}{} ", cursor::Goto(1, 1), clear::All)?;
 
+        let task_rec = command_receiver.clone();
         task::spawn(async move {
-            while let Ok(search_string) = receiver.recv().await {
-                let query = Query::builder().terms(vec![search_string]).build();
+            while let Ok(search_string) = task_rec.recv().await {
+                let query = Query::builder().terms(vec![search_string.clone()]).build();
                 let hits = remotes::dblp::DBLP::fetch(query).await;
                 if let Ok(hits) = hits {
-                    for (i, hit) in hits.into_iter().enumerate().take(5) {
-                        write!(
-                            std::io::stdout(),
-                            "{hide}{goto}{clear}{}",
-                            hit,
-                            hide = cursor::Hide,
-                            goto = cursor::Goto(1, i as u16 + 4),
-                            clear = clear::CurrentLine
-                        )
-                        .ok();
-                    }
-                    std::io::stdout().flush().ok();
+                    print_results(&hits, &search_string, None);
+                    hit_sender.send(hits).await;
+                    // let n = hits.len();
+                    // for (i, hit) in hits.into_iter().enumerate().take(5) {
+                    //     write!(
+                    //         std::io::stdout(),
+                    //         "{hide}{goto}{clear}{}",
+                    //         hit,
+                    //         hide = cursor::Hide,
+                    //         goto = cursor::Goto(1, i as u16 + 4),
+                    //         clear = clear::CurrentLine
+                    //     )
+                    //     .ok();
+                    // }
+
+                    // info(&format!("Found {} results!", n));
+
+                    // std::io::stdout().flush().ok();
                 }
             }
         });
@@ -52,26 +67,43 @@ impl Command for Interactive {
             let k = stdin.next();
 
             if let Some(Ok(key)) = k {
-                let action = handle_key(key, &state, &mut data)?;
-
+                while let Ok(hits) = hit_receiver.try_recv() {
+                    log::warn!("Fetched {} hits", hits.len());
+                    data.hits = hits;
+                }
+                log::warn!("Pressed key {:?}", key);
+                let action = handle_key(key, &mut data);
                 write!(
                     std::io::stdout(),
-                    "{hide}{goto}{clear}{}",
+                    "{hide}{goto}{clear}Search: {}",
                     data.search_term,
                     hide = cursor::Hide,
-                    goto = cursor::Goto(1, 2),
+                    goto = cursor::Goto(1, 1),
                     clear = clear::CurrentLine
                 )
                 .ok();
                 std::io::stdout().flush().ok();
 
-                update_state(&action, &mut state);
-                match action {
-                    Action::UpdateSearch => {
-                        while let Ok(_) = local_rec.try_recv() {}
-                        sender.try_send(data.search_term.clone())?
+                log::warn!("Current state {:?}", data.state);
+
+                if let Some(action) = action {
+                    match action {
+                        Action::UpdateSearch => {
+                            while let Ok(_) = command_receiver.try_recv() {}
+                            if !data.search_term.is_empty() {
+                                info(&String::from("Searching..."));
+                                command_sender.try_send(data.search_term.clone())?
+                            } else {
+                                clear_results()
+                            }
+                        }
+                        Action::UpdateSelection => {
+                            log::warn!("Update selection");
+                            print_results(&data.hits, &data.search_term, data.selected);
+                        }
+                        Action::Quit => break,
+                        _ => {}
                     }
-                    _ => break,
                 }
             }
         }
@@ -89,18 +121,78 @@ impl Command for Interactive {
     }
 }
 
-fn handle_key(key: Key, state: &State, data: &mut StateData) -> Result<Action> {
+fn print_results(results: &[PaperHit], search_term: &str, selected: Option<u16>) -> Result<()> {
+    let (width, height) = termion::terminal_size().unwrap_or((80, 20));
+    for (i, paper) in results.into_iter().enumerate().take(10) {
+        if Some(i as u16) == selected {
+            write!(
+                std::io::stdout(),
+                "{hide}{goto}{clear}{color}{}",
+                paper,
+                hide = cursor::Hide,
+                goto = cursor::Goto(1, i as u16 + 4),
+                clear = clear::CurrentLine,
+                color = color::Bg(color::Red),
+            )?;
+        } else {
+            write!(
+                std::io::stdout(),
+                "{hide}{goto}{clear}{}",
+                paper,
+                hide = cursor::Hide,
+                goto = cursor::Goto(1, i as u16 + 4),
+                clear = clear::CurrentLine,
+            )?;
+        }
+    }
+    std::io::stdout().flush()?;
+    Ok(())
+}
+
+fn handle_key(key: Key, data: &mut StateData) -> Option<Action> {
+    let state = data.state.clone();
     match (key, state) {
-        (Key::Esc | Key::Ctrl('c'), _) => Ok(Action::Quit),
+        (Key::Esc | Key::Ctrl('c'), _) => Some(Action::Quit),
         (Key::Char(c), State::Searching) => {
             data.search_term.push(c);
-            Ok(Action::UpdateSearch)
+            Some(Action::UpdateSearch)
         }
         (Key::Backspace, State::Searching) => {
             data.search_term.pop();
-            Ok(Action::UpdateSearch)
+            Some(Action::UpdateSearch)
         }
-        (_, _) => Ok(Action::Quit),
+        (Key::Down, State::Searching) => {
+            if data.hits.len() > 0 {
+                data.selected = Some(0);
+                data.state = State::Scrolling;
+                Some(Action::UpdateSelection)
+            } else {
+                None
+            }
+        }
+        (Key::Down, State::Scrolling) => {
+            let i = data.selected.unwrap();
+            if i < data.hits.len() as u16 - 1 {
+                *data.selected.as_mut().unwrap() = i + 1;
+            }
+            Some(Action::UpdateSelection)
+        }
+        (Key::Up, State::Scrolling) => {
+            let i = data.selected.unwrap();
+            if i > 0 {
+                *data.selected.as_mut().unwrap() = i - 1;
+            } else {
+                data.state = State::Searching;
+                data.selected = None;
+            }
+            Some(Action::UpdateSelection)
+        }
+        (Key::Char('s'), State::Scrolling) => {
+            data.state = State::Searching;
+            data.selected = None;
+            Some(Action::UpdateSelection)
+        }
+        (_, _) => Some(Action::Quit),
     }
 }
 
@@ -112,27 +204,63 @@ fn update_state(action: &Action, state: &mut State) {
     }
 }
 
+#[derive(Clone)]
 struct StateData {
     search_term: String,
+    selected: Option<u16>,
+    hits: Vec<PaperHit>,
+    state: State,
 }
 
 impl StateData {
     fn new() -> Self {
         Self {
             search_term: String::new(),
+            selected: None,
+            hits: vec![],
+            state: State::Searching,
         }
     }
 }
 
+#[derive(Debug, Clone)]
 enum State {
     Searching,
+    Displaying,
     Scrolling,
 }
 
 enum Action {
     UpdateSearch,
     ToSearch,
+    UpdateSelection,
     ToScroll,
-
     Quit,
+}
+
+fn clear_results() {
+    write!(
+        std::io::stdout(),
+        "{hide}{goto}{clear}",
+        hide = cursor::Hide,
+        goto = cursor::Goto(1, 2),
+        clear = clear::AfterCursor
+    )
+    .ok();
+    std::io::stdout().flush().ok();
+}
+
+fn info<I: Display>(item: &I) -> usize {
+    let buf = format!("{}", item);
+    write!(
+        std::io::stdout(),
+        "{hide}{goto}{clear}{}",
+        buf,
+        hide = cursor::Hide,
+        goto = cursor::Goto(1, 2),
+        clear = clear::CurrentLine
+    )
+    .ok();
+    std::io::stdout().flush().ok();
+    buf.len()
 }
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,30 @@ use clap::Parser;
 use cli::{Cli, Command};
 
 fn main() {
+    set_up_logging();
     if let Err(error) = Cli::parse().run() {
         println!("{}", error);
     }
+}
+fn set_up_logging() -> Result<(), fern::InitError> {
+    std::fs::create_dir_all("logs")?;
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "[{date}][{level}] {message}",
+                date = chrono::Local::now().format("%H:%M:%S"),
+                level = record.level(),
+                message = message
+            ));
+        })
+        .level(log::LevelFilter::Warn)
+        .chain(fern::log_file(format!(
+            "logs/{}.log",
+            chrono::Local::now().format("%d%m%Y-%H%M")
+        ))?)
+        .apply()?;
+
+    log::info!("Logger set up!");
+
+    Ok(())
 }
